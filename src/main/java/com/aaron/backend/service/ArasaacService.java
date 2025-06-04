f3a82bdc73d9e87f284dfbcf7d8d9b418bf27dd4
AaronGH09 <182010385+AaronGH09@users.noreply.github.com>
2025-06-04 10:47:44 +0200

    Update ArasaacService.java

--- src/main/java/com/aaron/backend/service/ArasaacService.java
+++ src/main/java/com/aaron/backend/service/ArasaacService.java
@@ -18,5 +18,4 @@ public interface ArasaacService {
 
     //Metodo para busca pictogramas relacionados con una palabra y devuelve sus ids.
     List<Integer> getPictogramIds(String word);
->>>>>>> 4818661 (Proyecto inicial conectado a repo existente)
 }
